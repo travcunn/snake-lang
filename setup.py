1de9baff8502b7246799e888c56f4e607333b4f0
Youssef Nasr <yoyo.monem22@gmail.com>
2022-01-18 18:00:20 +0000

    Change version of Snake installer to 1.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup  (
     name        = 'The Snake Programming Language\'s Simple Virtual Program Assembler',
-    version     = '1.0.1',
+    version     = '1.0.2',
     description = 'A simple virtual machine and program assembler',
     author = 'Travis Cunningham',
     author_email = 'yoyo.monem22@gmail.com',
